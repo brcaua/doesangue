91709a36f339a5d2ce83698a38355ade1c5f53bb
Breno Caua <progbrcaua@gmail.com>
2020-04-21 01:18:17 -0300

    Finalizando o front-end do site

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 // chamando o pacote express que está instalao na máquina e criando um servidor com "server"
 const express = require('express');
-
 const server = express();
 
 // configurando o servidor para apresentar arquivos estáticos
-server.use(express.static('/src/'));
+server.use(express.static('public'));
+
+//habilitar propriedade "body" do form
+server.use(express.urlencoded({ extended: true}))
 
 // configurando a template engine (motor de temas)
 const nunjucks = require('nunjucks');
 
 nunjucks.configure('./', {
   express: server,
+  noCache: true,
 });
 
 // Lista de doadores
@@ -35,7 +38,20 @@ const donors = [
 
 // configurando a apresentação da página 💻
 // o método "get" serve para pegar e mandar requisições, como no exemplo abaixo, no qual eu passo um parâmetro "/" e retorno um "res(resposta)" para o servidor
-server.get('/', (req, res) => res.render('/src/index.html', { donors }));
+server.get('/', (req, res) =>
+res.render("index.html", { donors })
+);
+
+server.post('/', (req, res) => {
+  const name = req.body.name
+  const blood = req.body.blood
+  //add valores ao array
+  donors.push({
+    name: name,
+    blood: blood,
+  })
+  return res.redirect('/')
+})
 
 // Iniciando o servidor..
 server.listen(3000, () => {
